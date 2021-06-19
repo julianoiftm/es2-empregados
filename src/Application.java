24b5bcb93f5ec0a45dd1c8a38cab43e041d0dbd7
julianoleo <50123832+julianoleo@users.noreply.github.com>
2021-06-19 18:32:10 -0300

    Ajustes metodos Empregado

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,4 @@
-import Empregado.Empregado;
-
 public class Application {
     public static void main(String[] args) {
-        Empregado emp1 = new Empregado();
-        emp1.setNome("José das Couves");
-        emp1.setHoras(40);
-        emp1.setValorPorHora(30.0);
-
-       System.out.println(emp1);
     }
 }
